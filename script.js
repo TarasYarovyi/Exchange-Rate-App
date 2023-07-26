b3290e4136881c7ce7109ad9a0647051aaba6c41
TarasYarovyi <yarovoy.taras@gmail.com>
2023-07-26 19:02:45 +0200

    Added queryselectors, main function updated

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 "use strict";
+const amountOne = document.querySelector(".amount-one");
+const amountTwo = document.querySelector(".amount-two");
+const currencyOne = document.querySelector("#currency-one");
+const currencyTwo = document.querySelector("#currency-two");
+const swap = document.querySelector(".swap");
+const rateInfo = document.querySelector(".rate-info");
 
-function calculate(amount, curencyA, curencyB) {
-  const requestURL = `https://api.exchangerate.host/convert?from=${curencyA}&to=${curencyB}&amount=${amount}`;
+swap.addEventListener("click", calculate);
+
+function calculate() {
+  const requestURL = `https://api.exchangerate.host/convert?from=${currencyOne.value}&to=${currencyTwo.value}&amount=${amountOne.value}&places=2`;
   const request = new XMLHttpRequest();
   request.open("GET", requestURL);
   request.responseType = "json";
   request.send();
   request.onload = function () {
-    const response = request.response;
-    console.log(response.result);
+    rateInfo.textContent = `Exchange rate: ${request.response.info.rate}`;
+    amountTwo.value = request.response.result;
   };
 }
-calculate(100, "EUR", "USD");
